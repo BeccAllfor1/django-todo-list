7703ca10d571183157e96d522c9dd52b1abeff10
Rebecca Allford <rebeccaallford1@gmail.com>
2024-06-19 09:25:08 +0000

    CSRF trusted populated with site addresses

--- todo_site/todo_site/settings.py
+++ todo_site/todo_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-20@b_86&(cf@*6n4qg_*09=8es$st8%6n#nkha*^p1veon7^!l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-beccallfor1-djangotodol-pdsecuefxqw.ws.codeinstitute-ide.net' ,
+ALLOWED_HOSTS = ['8000-beccallfor1-djangotodol-pdsecuefxqw.ws.codeinstitute-ide.net', 
 '.herokuapp.com']
 
 
@@ -81,7 +81,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.8000-beccallfor1-djangotodol-pdsecuefxqw.ws.codeinstitute-ide.net",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
