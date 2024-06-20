49041e2c9e4b036d4ca2ad837ff46ce7667db1e7
Rebecca Allford <rebeccaallford1@gmail.com>
2024-06-20 11:50:28 +0000

    debug

--- todo_site/settings.py
+++ todo_site/settings.py
@@ -98,6 +98,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.8000-beccallfor1-djangotodol-pdsecuefxqw.ws.codeinstitute-ide.net",
     "https://*.herokuapp.com"
 ]
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -134,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static', ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
