ef427b170d1e1f9f10544bc92906a9401ccaffb2
Rebecca Allford <rebeccaallford1@gmail.com>
2024-07-09 12:12:32 +0000

    added code to form to ensure allocated to person had to be completed

--- todo_site/settings.py
+++ todo_site/settings.py
@@ -24,8 +24,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-20@b_86&(cf@*6n4qg_*09=8es$st8%6n#nkha*^p1veon7^!l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
